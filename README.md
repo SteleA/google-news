23e0fb864f358ca4506ac9c01a370456d081bba5
Alex <alemik@live.se>
2016-02-04 23:47:26 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# google-news
+# Scrapes Google news
+
+Uses ES6
+
+`  
+import googleNews from 'google-news';
+
+googleNews({term: 'searchTerm', number: 10}, (err, news) => {
+    if(err) throw new Error(err);
+    console.log(news)
+  });
+`
---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import request from 'request';
+import cheerio from 'cheerio';
+
+
+const googleNews = ({ number = 10, term = ''}, cb) => {
+  request.get(`https://www.google.com/search?num=${number}&tbm=nws&q=${term}`, (err, response, html) => {
+    if(err) return cb(err);
+
+    const $       = cheerio.load(html);
+    let newsArr   = [];
+
+     $('.g').each(function(i, news){
+
+      const image   = $(news).find('img').attr('src');
+      const url     = $(news).find('a').attr('href').substring(7).split('&sa')[0];
+      const title   = $(news).find('h3').text();
+      const source  = $(news).find('h3').next().text().split('-')[0];
+      const time    = $(news).find('h3').next().text().split('-')[1];
+      const desc    = $(news).find('h3').next().next().text();
+
+      newsArr       = [ {image, url, title, source, time, desc} , ...newsArr];
+
+    });
+
+    return cb(null, newsArr);
+
+  });
+};
+
+
+export default googleNews;
